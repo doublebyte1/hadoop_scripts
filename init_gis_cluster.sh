f2e1ebe0dd68fbbccdfc6080d184de5b50bae23c
doublebyte <jo@doublebyte.net>
2015-02-13 14:28:03 +0100

    - ammended output path for jars

--- init_gis_cluster.sh
+++ init_gis_cluster.sh
@@ -5,9 +5,9 @@ cd /home/hadoop/esri-git
 
 wget -S -T 10 -t 5 --no-check-certificate -O /home/hadoop/esri-git/gis-tools-hadoop-2.0.jar https://github.com/Esri/gis-tools-for-hadoop/releases/download/v2.0/gis-tools-hadoop-2.0.jar
 
-wget -S -T 10 -t 5 --no-check-certificate -O /home/hadoop/esri-git/brickhouse-0.6.0.jar https://s3-eu-west-1.amazonaws.com/emr-gis-bdigital/bootstrap/brickhouse-0.6.0.jar
+wget -S -T 10 -t 5 --no-check-certificate -O /home/hadoop/brickhouse-0.6.0.jar https://s3-eu-west-1.amazonaws.com/emr-gis-bdigital/bootstrap/brickhouse-0.6.0.jar
 
-wget -S -T 10 -t 5 --no-check-certificate -O /home/hadoop/esri-git/csv-serde-1.1.2-0.11.0-all.jar https://s3-eu-west-1.amazonaws.com/emr-gis-bdigital/bootstrap/csv-serde-1.1.2-0.11.0-all.jar
+wget -S -T 10 -t 5 --no-check-certificate -O /home/hadoop/csv-serde-1.1.2-0.11.0-all.jar https://s3-eu-west-1.amazonaws.com/emr-gis-bdigital/bootstrap/csv-serde-1.1.2-0.11.0-all.jar
 
 cp /home/hadoop/esri-git/* /home/hadoop/hive/lib/
 cd /home/hadoop
